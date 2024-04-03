9a50c814b1492999d738cd254eb9c4be90c59a5c
Lucas Carvalho <locarvalho.lucas@outlook.com>
2024-04-03 16:26:57 -0300

    aplicação do framework AAA

--- login_test.rb
+++ login_test.rb
@@ -1,25 +1,26 @@
 require 'selenium-webdriver'
 
 class LoginTests
-  def setup
-    # Configurando o navegador para ser maximizado
+  def setup    
     options = Selenium::WebDriver::Chrome::Options.new(args: ['start-maximized'])
+    
     @driver = Selenium::WebDriver.for :chrome, options: options
     @driver.manage.timeouts.implicit_wait = 10
+    
     @wait = Selenium::WebDriver::Wait.new(timeout: 10)
   end
 
   def test_login
+    
+    # Arrange
     @driver.navigate.to "https://front.serverest.dev/login"
 
-    # Encontrar os elementos de usuário e senha e preencher com as credenciais
+    # Act
     @driver.find_element(id: 'email').send_keys('automation@email.com')
     @driver.find_element(id: 'password').send_keys('123456')
-
-    # Clicar no botão de login
     @driver.find_element(xpath: '//*[@id="root"]/div/div/form/button').click
 
-    # Esperar algum elemento da próxima página para confirmar o login
+    # Assert
     @wait.until { @driver.find_element(xpath: '//*[@id="navbarTogglerDemo01"]/form/button') }
     puts 'Login realizado com sucesso!'
   end
